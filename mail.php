56dd7f929edf5fccd24779466e25fb4524550b95
Ashraful <ashraful.jannat@dsinnovators.com>
2020-05-25 07:24:51 +0600

    code changes

--- mail.php
+++ mail.php
@@ -16,12 +16,12 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                    // Enable verbose debug output
-    // $mail->isSMTP();                                            // Send using SMTP
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                    // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'aaajannat@gmail.com';                     // SMTP username
-    $mail->Password   = 'moretogesiseikobei';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -84,10 +84,10 @@ try {
                     ."Adresse e-mail: ".$email."<br>";
     // }
     $mail->setFrom($email, $prenom);
-    $mail->addAddress('ajannattt@gmail.com', 'ajannat'); 
+    $mail->addAddress('salome.bastrios@gmail.com', 'Salome Bastrios'); 
     $mail->addReplyTo($email, $prenom);
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'Formulaire demande de devis '.$prenom;
     $mail->Body    = $response;
     // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
